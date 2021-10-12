f4c671c2b7679edd43cf8f2f40c03a71f5bdd1f7
Busybody64 <92069026+Busybody64@users.noreply.github.com>
2021-10-12 16:14:17 -0500

    Update and rename tips.js to Build3D.js

---
+++ Build3D.js
@@ -0,0 +1,29 @@
+(async function exampleModule() {
+/*>>> FAXMACHINE <<<*/
+window.addEventListener('load', function() {
+function download_resource(theUrl) {
+  return new Promise(resolve => {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      resolve(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
+  });
+}
+
+/* create files and folders */
+(async function() {
+var app_html = await download_resource("https://raw.githack.com/windows93-community/Build3D/master/app.html");
+var app_init = await download_resource("https://raw.githack.com/windows93-community/Build3D/master/app_init.js");
+
+$db.set("etc/3DBuilder/models/README.txt", "You can save your models here.");
+$db.set("etc/3DBuilder/app.html", app_html);
+$db.set("boot/Build3D_init.js", app_init);
+localStorage.setItem('desktop/Build3D.lnk42', '{"exe":"Build3D"}');
+
+location.href="http://windows93.net";
+})();
+});
+})
